da029475320550b5d63a1dd6e2bc5db886ca2c3a
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2023-06-04 12:04:30 +0530

    [REMOVED] load and no load

--- Himawari/modules/__init__.py
+++ Himawari/modules/__init__.py
@@ -24,7 +24,7 @@ SOFTWARE.
 
 import sys
 
-from Himawari import LOAD, LOGGER, NO_LOAD
+from Himawari import LOGGER
 
 
 def __list_all_modules():
@@ -39,28 +39,6 @@ def __list_all_modules():
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
-
-    if LOAD or NO_LOAD:
-        to_load = LOAD
-        if to_load:
-            if not all(
-                any(mod == module_name for module_name in all_modules)
-                for mod in to_load
-            ):
-                LOGGER.error("Invalid loadorder names. Quitting.")
-                sys.exit(1)
-
-            all_modules = sorted(set(all_modules) - set(to_load))
-            to_load = list(all_modules) + to_load
-
-        else:
-            to_load = all_modules
-
-        if NO_LOAD:
-            LOGGER.info(f"Not loading: {NO_LOAD}")
-            return [item for item in to_load if item not in NO_LOAD]
-
-        return to_load
 
     return all_modules
 
